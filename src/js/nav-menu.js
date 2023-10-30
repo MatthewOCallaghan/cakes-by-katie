2a1bce6fb1292b170872f97d5e6873a4637e3c7c
Matthew O'Callaghan <matthew.ocallaghan@manything.com>
2023-10-30 23:06:25 +0000

    Support navbar submenus

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -1,3 +1,7 @@
+/* -------------------------------------------------------------------------- */
+/*                                 Mobile nav                                 */
+/* -------------------------------------------------------------------------- */
+
 const NAV_OPEN_CLASS = 'nav-open';
 
 // Add onclick to nav toggler
@@ -11,5 +15,42 @@ document.querySelectorAll('nav a').forEach(element => {
 		if (element.href.includes(window.location.pathname)) {
 			document.body.classList.remove(NAV_OPEN_CLASS);
 		}	
+	}
+});
+
+
+/* -------------------------------------------------------------------------- */
+/*                                 Desktop nav                                */
+/* -------------------------------------------------------------------------- */
+
+const SUBMENU_OPEN_HEADER_ATTRIBUTE = 'data-nav-submenu';
+
+document.querySelectorAll('#main-links > li').forEach((li, index) => {
+	const button = li.querySelector('button');
+	if (button) {
+		const submenuId = index + 1;
+		const header = document.querySelector('header');
+		button.onclick = () => {
+			const submenuOpen = parseInt(header.getAttribute(SUBMENU_OPEN_HEADER_ATTRIBUTE));
+			if (submenuOpen === submenuId) {
+				header.removeAttribute(SUBMENU_OPEN_HEADER_ATTRIBUTE);
+			} else {
+				header.setAttribute(SUBMENU_OPEN_HEADER_ATTRIBUTE, submenuId);
+			}
+		}
+
+		let timeout;
+		li.onmouseover = () => {
+			clearTimeout(timeout);
+			header.setAttribute(SUBMENU_OPEN_HEADER_ATTRIBUTE, submenuId);
+		}
+		li.onmouseleave = () => {
+			timeout = setTimeout(() => {
+				const submenuOpen = parseInt(header.getAttribute(SUBMENU_OPEN_HEADER_ATTRIBUTE));
+				if (submenuOpen === submenuId) {
+					header.removeAttribute(SUBMENU_OPEN_HEADER_ATTRIBUTE);
+				}
+			}, 50);
+		}
 	}
 });
