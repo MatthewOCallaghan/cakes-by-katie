f7490965a5a0774177c6d85d59ad90007e89ae3d
Matthew O'Callaghan <matthew.ocallaghan@manything.com>
2023-12-30 16:57:59 +0000

    Add products to navbar

--- src/js/imageSlideshow.js
+++ src/js/imageSlideshow.js
@@ -5,11 +5,13 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     // Automatic slideshow
     autoplay: {
-        delay: 1000, // Ms per slide
+        delay: 500, // Ms per slide
         disableOnInteraction: false,
         pauseOnMouseEnter: true
     },
     speed: 1500,
     slidesPerView: 'auto',
-    spaceBetween: 5
+    spaceBetween: 5,
+    // Active slide is in centre rather than far left
+    centeredSlides: true,
 });
